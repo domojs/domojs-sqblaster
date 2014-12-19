8bca035b44ca6b61b5b759601421216e712afe30
npenin <nicolas.penin@dragon-angel.fr>
2014-12-19 11:32:18 +0100

    initial commit

---
+++ device.js
@@ -0,0 +1,44 @@
+
+deviceTypes['sq-blaster']={
+    name:'sq-blaster',
+    onChange:function(){
+        if(!deviceTypes['.sq-blaster'] || deviceTypes['.sq-blaster'].devices)
+            $.getJSON('/api/device/category/.sq-blaster', function(devices){ deviceTypes['.sq-blaster'].devices=devices; $('#commands').empty(); deviceTypes['sq-blaster'].onChange(); });
+        var device=$('<li class="form-group">')
+            .append('<label class="col-sm-2 control-label" for="blaster">Blast through</label>')
+            .append('<div class="col-sm-10"><select class="commandsUrl form-control" placeholder="Url" id="blaster" name="blaster"></select></div>')
+            .appendTo('#commands');
+        if(deviceTypes['sq-blaster'].devices)
+            $.each(deviceTypes['sq-blaster'].devices, function(index,item){
+                device.find('select').append('<option>'+item.name+'</option>');
+            });
+        $('<li class="form-group">')
+            .append('<label class="col-sm-2 control-label" for="device">Definition file</label>')
+            .append('<div class="col-sm-10"><input type="file" class="form-control" placeholder="Device xml file" name="device" /></div>')
+            .appendTo('#commands').find('input').on('change', function(event){
+                window.deviceFile=event.target.files[0];
+            });
+        return 'dynamic';
+    },
+    onSave:function(data){
+        $('form')
+            .removeAttr( "enctype", "multipart/form-data" )
+            .removeAttr( "encoding", "multipart/form-data" );
+        debugger;
+        data.append('device', window.deviceFile);
+        data.append('blaster', $('#blaster').val())
+    },
+    onServerSave:function(device, body, callback){
+        var xml2js=$('xml2js');
+        body.device=body.device.toString('utf8');
+        xml2js.parseString(body.device, function(error, dev){
+            if(error)
+                return console.log(error);
+            console.log(dev);
+            $.each(dev.SQCommandInfo.command_info, function(index, item){
+                device.commands[item.$.command_name]={type:'POST', contentType:'application/xml', data:'<docommand key="'+device.name+'" repeat= "0" seq="0" command="'+item.$.command_name+'" ir_data="'+item.$.ir_data+'" ch="0" />', url:'http://'+body.blaster+'/docmnd.xml', crossDomain:true, headers:{'Connection':'close'}};
+            });
+            device.subDevices=[{name:'power', commands:{on:function(){ return item['POWER ON']}, off:function(){ return item['POWER OFF']; }, toggle:function(){ return item['POWER TOGGLE']}}}];
+        });
+    }
+}; 
---
+++ index.js
@@ -0,0 +1,18 @@
+
+var browser=$('mdns').createBrowser($('mdns').tcp('http'));
+
+browser.on('serviceUp', function(service){
+    if(service.host.startsWith('sq-blaster'))
+    {
+        console.log(service);
+        $.device({type:'.sq-blaster', category:'sq-blaster', name:service.host});
+    }
+});
+browser.start();
+
+exports.init=function(config)
+{
+    $.each($('./modules/sq-blaster/devices.json'), function(index, device){
+        $.device(device);
+    });
+};
